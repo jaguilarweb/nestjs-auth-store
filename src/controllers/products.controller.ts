35a11249c6af9007608b3c4ff412eb6b45ff70c7
Jenny Aguilar <contact@jaguilarweb.com>
2023-04-08 13:13:30 +0200

    feat: Add product remove endpoint

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Put, Body, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  Query,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -50,6 +59,14 @@ export class ProductsController {
       message: 'Update product',
       id,
       payload,
+    };
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return {
+      message: 'Delete product',
+      id,
     };
   }
 }
