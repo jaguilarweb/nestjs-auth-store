e6c75280ff7498b20235b18ea4d4ef64fc956005
Jenny Aguilar <contact@jaguilarweb.com>
2023-04-08 19:35:30 +0200

    refactor: Change product dto file to add validators

--- src/dtos/product.dtos.ts
+++ src/dtos/product.dtos.ts
@@ -1,8 +1,20 @@
+import { IsString, IsNumber, IsUrl, IsNotEmpty } from 'class-validator';
+
 export class CreateProductDtos {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+  @IsNumber()
+  @IsNotEmpty()
   readonly price: number;
+  @IsNumber()
+  @IsNotEmpty()
   readonly stock: number;
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
 }
 
