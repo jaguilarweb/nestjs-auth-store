823db296dbfa229f69c595128d8d34433dd0f8f2
Jenny Aguilar <contact@jaguilarweb.com>
2023-04-08 20:15:13 +0200

    refactor: Change main file to set validation

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //Ignora el atributo adicional
+      forbidNonWhitelisted: true, //Alerta que hay un atributo adicional
+      /* transform: true, */
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
