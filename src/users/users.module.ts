4589d5f9d5aa32d905a26aa95358369a48a534a3
Jenny Aguilar <contact@jaguilarweb.com>
2023-04-12 14:15:27 +0200

    refactor: Add user controler and service into user module file

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controllers/users.controller';
+import { CustomerController } from './controllers/customers.controllers';
 import { UsersService } from './services/users.service';
+import { CustomersService } from './services/customers.service';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService],
+  controllers: [UsersController, CustomerController],
+  providers: [UsersService, CustomersService],
 })
 export class UsersModule {}
