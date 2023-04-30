0d25372e3b0829845d41a18a50c75e87119ba0f7
Jenny Aguilar <contact@jaguilarweb.com>
2023-04-30 13:34:37 +0200

    feat: Add an expose option from class-transformer to show data in order entity

--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -6,6 +6,7 @@ import {
   ManyToOne,
   OneToMany,
 } from 'typeorm';
+import { Exclude, Expose } from 'class-transformer';
 
 import { CustomerEntity } from './customer.entity';
 import { OrderItem } from './order-item.entity';
@@ -29,6 +30,34 @@ export class OrderEntity {
   @ManyToOne(() => CustomerEntity, (customer) => customer.orders)
   customer: CustomerEntity;
 
+  @Exclude() //Si pongo el expose pierdo la referencia a item
   @OneToMany(() => OrderItem, (item) => item.order)
   items: OrderItem[];
+
+  @Expose()
+  get products() {
+    if (this.items) {
+      return this.items
+        .filter((item) => !!item) //Que todo sea valido o item !=== null //Cuidado con los boolean
+        .map((item) => ({
+          ...item.product,
+          quantity: item.quantity,
+          itemId: item.id,
+        }));
+    }
+    return [];
+  }
+
+  @Expose()
+  get total() {
+    if (this.items) {
+      return this.items
+        .filter((item) => !!item) //Que todo sea valido o item !=== null //Cuidado con los boolean
+        .reduce((total, item) => {
+          const totalItem = item.product.price * item.quantity;
+          return total + totalItem;
+        }, 0);
+    }
+    return 0;
+  }
 }
