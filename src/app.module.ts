e0c0102f5f68bb530b9ee16d9bfcf6a39ba61f28
Jenny Aguilar <contact@jaguilarweb.com>
2023-05-02 14:11:26 +0200

    refactor: Change to read environment variables from the guard file

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,7 +25,7 @@ import config from './config';
           .valid('development', 'production', 'test')
           .default('development'),
         PORT: Joi.number().default(3000), */
-        API_KEY: Joi.number().required(),
+        API_KEY: Joi.string().required(),
         DATABASE_NAME: Joi.string().required(),
         DATABASE_PORT: Joi.number().required(),
       }),
--- src/auth/guards/apikey.guard.ts
+++ src/auth/guards/apikey.guard.ts
@@ -3,16 +3,22 @@ import {
   ExecutionContext,
   Injectable,
   UnauthorizedException,
+  Inject,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { ConfigType } from '@nestjs/config';
 
+import config from '../../config';
 import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 import { Request } from 'express';
 
 @Injectable()
 export class ApikeyGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(
+    private reflector: Reflector,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
 
   canActivate(
     context: ExecutionContext,
@@ -23,7 +29,7 @@ export class ApikeyGuard implements CanActivate {
     }
     const request = context.switchToHttp().getRequest<Request>(); //Obtenemos el request
     const authHeader = request.header('Auth');
-    const isAuth = authHeader === '1234';
+    const isAuth = authHeader === this.configService.apiKey;
     if (!isAuth) {
       throw new UnauthorizedException('Not allow');
     }
